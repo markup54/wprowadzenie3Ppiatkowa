09562a9a7a525b0938871976e19a7479c96e834a
markup54 <marzenakupka@tlen.pl>
2023-09-08 10:10:37 +0200

    zbiór na losowane liczby

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +8,23 @@ public class Main {
         System.out.println("losowanie 6 liczb całkowitej z zakresu od 1 do 100");
         int zmienna =10; //typ prosty, tylko zmienna
         Integer zmienna2 =20; //typ złozny wielka litera obiekt klasy Integer metody
+/*
+tablica musi mieć zdefiniowany rozmiar,
+nie można go zmienić w trakcie działania programu
+może przechowywać typy proste i złożone
+elementy indeksowane mogą się powtarzać
 
-        int wylosowaneLiczby[] = new int[6];
-        for (int i = 0; i < wylosowaneLiczby.length; i++) {
-            wylosowaneLiczby[i] = (int)(Math.random()*10+1);
+kolekcje
+nie deklarujemy rozmiaru, rozmiar kolekcji może się zmieniać w trakcie działania programu
+listy List ArrayList, LinkedList
+elementy indeksowane, każdy element ma swoje miejsce, elementy mogą się powtarzac
+zbiory Set HashSet
+zazwyczaj elementy nie sa indeksowane, elementy nie powtarzają się
+słowniki Map para klucz wartosc
+ */
+        HashSet<Integer> wylosowaneLiczby = new HashSet<>();
+        while(wylosowaneLiczby.size()<6) {
+            wylosowaneLiczby.add((int)(Math.random()*10+1));
 
         }
         for (int wartoscWylosowana:wylosowaneLiczby) {
