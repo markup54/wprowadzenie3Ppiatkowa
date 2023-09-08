500a25efbc5ffb7777724d73372d7e3386bf5fc1
markup54 <marzenakupka@tlen.pl>
2023-09-08 08:32:40 +0200

    typy proste, losowanie liczby

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj na losowaniu liczb");
+        //komentarz jednoliniowy
+
+        /*
+        komentarz wieloliniowy
+         */
+        System.out.println("losowanie liczby całkowitej z zakresu od 1 do 100");
+        int losowanaLiczba = (int)(Math.random()*100+1);
+        //Math.random() -losuje liczbę z zakresu<0,1)
+        /*typy proste
+        byte short int long - typy calkowite
+        float double rzeczywiste
+        boolean
+        char
+        rzutowanie rozszezrające - domyślnie
+        rzutowanie zawężające np z double (int)
+        */
+        System.out.println(losowanaLiczba);
     }
 }
